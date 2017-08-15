a731d3d58d7561af66f27d720ef0e09c6922a7da
651499253@.com <651499253@.com>
2017-08-16 01:27:12 +0800

    新增显示列维护窗体

---
+++ ShoesOrderPrint/ShoesOrderPrint/FrmColumns.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ShoesOrderPrint
+{
+    partial class FrmColumns
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txButton1 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.SuspendLayout();
+            // 
+            // txButton1
+            // 
+            this.txButton1.Image = null;
+            this.txButton1.Location = new System.Drawing.Point(252, 407);
+            this.txButton1.Name = "txButton1";
+            this.txButton1.Size = new System.Drawing.Size(100, 28);
+            this.txButton1.TabIndex = 0;
+            this.txButton1.Text = "保存";
+            this.txButton1.UseVisualStyleBackColor = true;
+            this.txButton1.Click += new System.EventHandler(this.txButton1_Click);
+            // 
+            // FrmColumns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 441);
+            this.Controls.Add(this.txButton1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "FrmColumns";
+            this.Text = "维护列";
+            this.Load += new System.EventHandler(this.FrmColumns_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TX.Framework.WindowUI.Controls.TXButton txButton1;
+
+    }
+}
---
+++ ShoesOrderPrint/ShoesOrderPrint/FrmColumns.cs
@@ -0,0 +1,100 @@
+﻿using ShoesOrderPrint.BLL;
+using ShoesOrderPrint.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TX.Framework.WindowUI.Controls;
+using TX.Framework.WindowUI.Forms;
+
+namespace ShoesOrderPrint
+{
+    public partial class FrmColumns : BaseForm
+    {
+        /// <summary>
+        /// 表示维护控件
+        /// </summary>
+        List<MColumnStyle> m_List;
+        /// <summary>
+        /// 维护列业务逻辑类
+        /// </summary>
+        ColumnStyleBLL m_ColumnStyleBLL = new ColumnStyleBLL();
+        public FrmColumns()
+        {
+            InitializeComponent();
+        }
+        public FrmColumns(List<MColumnStyle> myList)
+        {
+            m_List = myList;
+            InitializeComponent();
+        }
+
+        private void FrmColumns_Load(object sender, EventArgs e)
+        {          
+            int LocationY = 40;
+            foreach (MColumnStyle columnStyle in m_List)
+            {
+                //创建CheckBox控件
+                TXCheckBox myCheckBox = CreateCheckBox(columnStyle);
+                myCheckBox.Location = new Point(this.Width/2-myCheckBox.Width/2, LocationY);
+                this.Controls.Add(myCheckBox);
+                LocationY += 30;
+            }
+        }
+        /// <summary>
+        /// 创建CheckBox
+        /// </summary>
+        /// <param name="columnStyle"></param>
+        private TXCheckBox CreateCheckBox(MColumnStyle columnStyle)
+        {
+            TXCheckBox myCheckBox = new TXCheckBox();
+            myCheckBox.BackColor = Color.Transparent;
+            myCheckBox.Text = columnStyle.ColumnCaption;
+            if (columnStyle.ColumnVisible == 1)
+                myCheckBox.Checked = true;
+            else
+                myCheckBox.Checked = false;
+            myCheckBox.Tag = columnStyle;
+            return myCheckBox;
+        }
+
+        //保存
+        private void txButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Control control in this.Controls)
+                {
+                    TXCheckBox myCheckBox = control as TXCheckBox;
+                    if (myCheckBox == null)
+                        continue;
+                    MColumnStyle mColumnStyle = myCheckBox.Tag as MColumnStyle;
+                    if (mColumnStyle == null)
+                        continue;
+                    int visible = 0;
+                    if (myCheckBox.Checked)
+                        visible = 1;
+                    else
+                        visible = 0;
+                    if (visible != mColumnStyle.ColumnVisible)
+                    {
+                        mColumnStyle.ColumnVisible = visible;
+                        m_ColumnStyleBLL.Update(mColumnStyle);
+                    }                
+                }
+                this.Info("保存成功！");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+
+                this.Warning(ex.Message);
+            }
+        }
+    }
+}
